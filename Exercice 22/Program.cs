091e567db666c4b56804f7be36a541c46ae5ef79
LeoMe <LeoMe@DESKTOP-F52R34A>
2023-09-20 21:12:18 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice 22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // déclaration
+            float note;
+            int nbsup = 0;
+            // boucle sur la saisie des 10 notes
+            for(int k = 1; k <= 10; k++)
+            {
+                Console.Write("Entrez une note = ");
+                note = float.Parse(Console.ReadLine());
+                // affichage des notes supérieures ou égales à 10
+                if (note >= 10)
+                {
+                    nbsup++;
+                }
+            }
+            // affichage des notes supérieures et inférieurs ou égales à 10
+            Console.WriteLine("Nombres de notes >= 10 : " + nbsup);
+            Console.WriteLine("Nombres de notes < 10 : " + (10 - nbsup));
+            Console.ReadLine();
+        }
+    }
+}
